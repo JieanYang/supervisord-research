33b8c1c22f713f49308a82ebd8abd3db409197d2
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-30 11:19:17 +0100

    Add test files

---
+++ 2022-12-22-first_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "start"
+echo "hello yang"
+echo "end"
---
+++ 2022-12-22-second_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "start sleep 30s"
+# sleep 5
+# sleep 60
+# echo "1m..."
+# sleep 60
+# echo "2m..."
+# sleep 60
+# echo "3m..."
+# sleep 60
+# echo "4m..."
+# sleep 60
+sleep 30
+echo "30s..."
+echo "end sleep 30s"
+echo "hello yang"
+echo "end"
