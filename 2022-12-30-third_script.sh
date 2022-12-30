1a153cc4ea575879e172056c6a5ff2c8b04ace14
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-30 23:04:35 +0100

    Update files for test

--- 2022-12-30-third_script.sh
+++ 2022-12-30-third_script.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-echo "start"
-echo "hello yang"
-echo "end"
-echo "2022-12-30-third_script" >> "third_script_output.txt"
+
+# echo "start"
+# echo "hello yang"
+# echo "end"
+# echo "2022-12-30-third_script" >> "third_script_output.txt"
+
+# Update pacakges
+sudo yum update -y
+
+# Install git
+sudo yum install git -y
+
+# Install go
+sudo yum install golang -y
+
+# Pull github
+cd ~
+git clone https://github.com/JieanYang/supervisord-research.git
+cd supervisord-research
+
+# Build supervisord
+go build
+
+# Start service
+./supervisord -c supervisor-linux.conf
