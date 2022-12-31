cca7550271bb9af5280f68a9654d2599a496be0c
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-31 10:15:38 +0100

    Update 10

--- go_test_api/go_test_api.go
+++ go_test_api/go_test_api.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", rootHandler)
 
-	err := http.ListenAndServe(":9002", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
