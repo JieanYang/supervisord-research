810883ed74363ff16410d642eecaf6c9091da4bc
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-01-02 11:53:23 +0100

    Start supervisord in supervisord-windows.config

--- service.go
+++ service.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/kardianos/service"
 	log "github.com/sirupsen/logrus"
 )
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Go - Hello World</h1>")
+}
 
 // ServiceCommand install/uninstall/start/stop supervisord service
 type ServiceCommand struct {
@@ -23,6 +28,12 @@ func (p *program) Start(s service.Service) error {
 
 func (p *program) run() {
 	// Do work here
+	http.HandleFunc("/", rootHandler)
+
+	err := http.ListenAndServe(":9001", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // Stop supervised service
