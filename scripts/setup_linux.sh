bfa174187a0042c4e4fa97e33b6ae226e302a090
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-01-16 19:27:21 +0100

    Update scripts files

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,20 +1,26 @@
-Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned -Force
-Get-ExecutionPolicy -List
+#!/bin/bash
 
-$script = New-Object Net.WebClient
-$script | Get-Member
-$script.DownloadString("https://chocolatey.org/install.ps1")
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-choco upgrade chocolatey
+# echo "start"
+# echo "hello yang"
+# echo "end"
+# echo "2022-12-30-third_script" >> "third_script_output.txt"
 
-choco install -y golang git.install
-# $env:Path += 'C:\Program Files\Git\bin\git.exe;'
-# $env:Path += 'C:\Program Files\Go\bin\go.exe;'
+# Update pacakges
+# sudo yum update -y
 
-New-Alias -Name git -Value 'C:\Program Files\Git\bin\git.exe'
-New-Alias -Name go -Value 'C:\Program Files\Go\bin\go.exe'
+# Install git
+winget install --id Git.Git -e --source winget
+git version
 
-git clone --branch dev https://github.com/JieanYang/supervisord-research.git
+# Fetch Go repository
+git clone https://go.googlesource.com/go goroot
+cd goroot
+# git checkout <tag>
+
+# Install go
+cd src
+./all.bat
+go version
 
 # Pull github
 cd ~
@@ -30,7 +36,11 @@ else
   ###  Control will jump here if $DIR does NOT exists ###
   # echo "Error: ${DIR} not found. Can not continue."
   # exit 1
+<<<<<<< HEAD
   git clone --branch service-fix https://github.com/JieanYang/supervisord-research.git
+=======
+  git clone --branch dev https://github.com/JieanYang/supervisord-research.git
+>>>>>>> 7590bc7 (Update scripts files)
   cd supervisord-research
 fi
 
@@ -40,9 +50,20 @@ if [ -f "$FILE" ]; then
     rm $FILE
 fi
 go build
-go build -o ./go_test_api/go_test_api.exe ./go_test_api/go_test_api.go
 
-New-NetFirewallRule -DisplayName 'Go-application' -Profile @('Domain', 'Public','Private') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('9001')
+FILE=./go_test_api/go_test_api
+if [ -f "$FILE" ]; then
+    rm $FILE
+fi
+go build -o ./go_test_api/go_test_api.exe ./go_test_api/go_test_api.go
+chmod +x ./go_test_api/go_test_api
+
+# Copy binary app
+FILE=/usr/local/bin/go_test_api
+if [ -f "$FILE" ]; then
+    sudo rm $FILE
+fi
+sudo cp ./go_test_api/go_test_api /usr/local/bin/go_test_api
 
 sudo mkdir /supervisord
 
