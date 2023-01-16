22abb4c290267436a5da12c7bde74b050dce462e
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-01-16 18:50:14 +0100

    Create scripts/setup_windows.ps1

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned -Force
+Get-ExecutionPolicy -List
 
-# echo "start"
-# echo "hello yang"
-# echo "end"
-# echo "2022-12-30-third_script" >> "third_script_output.txt"
+$script = New-Object Net.WebClient
+$script | Get-Member
+$script.DownloadString("https://chocolatey.org/install.ps1")
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+choco upgrade chocolatey
 
-# Update pacakges
-sudo yum update -y
+choco install -y golang git.install
+# $env:Path += 'C:\Program Files\Git\bin\git.exe;'
+# $env:Path += 'C:\Program Files\Go\bin\go.exe;'
 
-# Install git
-sudo yum install git -y
-git version
+New-Alias -Name git -Value 'C:\Program Files\Git\bin\git.exe'
+New-Alias -Name go -Value 'C:\Program Files\Go\bin\go.exe'
 
-# Install go
-sudo yum install golang -y
-go version
+git clone --branch dev https://github.com/JieanYang/supervisord-research.git
 
 # Pull github
 cd ~
@@ -40,20 +40,9 @@ if [ -f "$FILE" ]; then
     rm $FILE
 fi
 go build
+go build -o ./go_test_api/go_test_api.exe ./go_test_api/go_test_api.go
 
-FILE=./go_test_api/go_test_api
-if [ -f "$FILE" ]; then
-    rm $FILE
-fi
-go build -o ./go_test_api/go_test_api ./go_test_api/go_test_api.go
-chmod +x ./go_test_api/go_test_api
-
-# Copy binary app
-FILE=/usr/local/bin/go_test_api
-if [ -f "$FILE" ]; then
-    sudo rm $FILE
-fi
-sudo cp ./go_test_api/go_test_api /usr/local/bin/go_test_api
+New-NetFirewallRule -DisplayName 'Go-application' -Profile @('Domain', 'Public','Private') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('9001')
 
 sudo mkdir /supervisord
 
