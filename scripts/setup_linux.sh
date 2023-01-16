c0fc0101e6f917f35b2ca4ef49039e63da4b5d8b
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-01-16 18:50:14 +0100

    Create scripts/setup_windows.ps1

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,59 +1,26 @@
-#!/bin/bash
+Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned -Force
+Get-ExecutionPolicy -List
 
-# echo "start"
-# echo "hello yang"
-# echo "end"
-# echo "2022-12-30-third_script" >> "third_script_output.txt"
+$script = New-Object Net.WebClient
+$script | Get-Member
+$script.DownloadString("https://chocolatey.org/install.ps1")
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+choco upgrade chocolatey
 
-# Update pacakges
-sudo yum update -y
+choco install -y golang git.install
+# $env:Path += 'C:\Program Files\Git\bin\git.exe;'
+# $env:Path += 'C:\Program Files\Go\bin\go.exe;'
 
-# Install git
-sudo yum install git -y
-git version
+New-Alias -Name git -Value 'C:\Program Files\Git\bin\git.exe'
+New-Alias -Name go -Value 'C:\Program Files\Go\bin\go.exe'
 
-# Install go
-sudo yum install golang -y
-go version
+git clone --branch dev https://github.com/JieanYang/supervisord-research.git
 
-# Pull github
-cd ~
-DIR=~/supervisord-research
-if [ -d "$DIR" ]; then
-  ### Take action if $DIR exists ###
-  # echo "Installing config files in ${DIR}..."
-  cd supervisord-research
-  git add .
-  git reset --h HEAD
-  git pull
-else
-  ###  Control will jump here if $DIR does NOT exists ###
-  # echo "Error: ${DIR} not found. Can not continue."
-  # exit 1
-  git clone --branch dev https://github.com/JieanYang/supervisord-research.git
-  cd supervisord-research
-fi
-
-# Build supervisord
-FILE=./supervisord
-if [ -f "$FILE" ]; then
-    rm $FILE
-fi
+cd supervisord-research
 go build
+go build -o ./go_test_api/go_test_api.exe ./go_test_api/go_test_api.go
 
-FILE=./go_test_api/go_test_api
-if [ -f "$FILE" ]; then
-    rm $FILE
-fi
-go build -o ./go_test_api/go_test_api ./go_test_api/go_test_api.go
-chmod +x ./go_test_api/go_test_api
-
-# Copy binary app
-FILE=/usr/local/bin/go_test_api
-if [ -f "$FILE" ]; then
-    sudo rm $FILE
-fi
-sudo cp ./go_test_api/go_test_api /usr/local/bin/go_test_api
+New-NetFirewallRule -DisplayName 'Go-application' -Profile @('Domain', 'Public','Private') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('9001')
 
 # Start service
 # ./supervisord -c supervisord-linux.conf -d
